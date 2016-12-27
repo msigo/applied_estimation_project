e015cd0003dfd47d67a46afe0187004b2fcd21a2
marc.sigo@hotmail.com <marcsigonius@Marcs-MBP.lan>
2016-12-27 14:45:44 +0100

    Updated so that the measurement uses the Hough Transform

---
+++ calculate_association_hough.m
@@ -0,0 +1,38 @@
+function [outlier,likelihood] = calculate_association_hough(particles,pic,pos_stD,centers,lambda_psi)
+image_height = size(pic, 1);
+image_width = size(pic, 2);
+
+%number of particles 
+N = size(particles,2);
+
+likelihood = zeros(1,N);
+
+A = sqrt(2 * pi) * pos_stD;
+B =  - 0.5 / (pos_stD.^2);
+
+particles = round(particles);
+if (size(centers,1)==0)
+    likelihood = 1/N*ones(1,N);
+    outlier = ones(1,N);
+    return 
+end
+for k = 1:N
+    
+    m = particles(1,k);
+    n = particles(2,k);
+    
+    I = (m >= 1 & m <= image_height);
+    J = (n >= 1 & n <= image_width);
+    
+    if I && J
+        D = (abs(particles(2:-1:1,k)'-centers))';
+        
+        D2 = D' * D;
+        
+        likelihood(k) = A*exp(D2*B);
+    else
+        
+        likelihood(k) = 0;
+    end
+outlier = likelihood<=lambda_psi;
+end 
